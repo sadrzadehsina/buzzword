c49f46400eb033f8af9c77aa0211848468636029
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2021-02-19 13:10:38 +0330

    Remove dead lines

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+#!/usr/bin/env node
+
 const importJsx = require('import-jsx');
 process.env.FORCE_COLOR = '1';
 
 process.stdout.write('\x1B[2J\x1B[3J\x1B[H');
 importJsx('./buzzword');
-// setTimeout(() => importJsx('./buzzword.js') , 500);
